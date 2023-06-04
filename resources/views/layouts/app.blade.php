e92dbd076db375b67baa531254c4203af95f9971
mahendraadkri <mahendrachhetri500@gmail.com>
2023-06-04 10:06:56 +0545

    AdminCRUD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,34 +3,49 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+     
+        @vite(['resources/js/app.js', 'resources/css/app.css'])
+        <script src="{{asset('datatable/jquery-3.6.0.js')}}"></script>
+        <link rel = "stylesheet" href="{{asset('datatable/datatables.css')}}">
+        <script src = "{{asset('datatable/datatables.js')}}"></script>
+    
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+        <div class="flex">
+            <div class="w-56 fixed top-0 left-0 bottom-0 bg-gray-200 shadow-lg shadow-red-300">
+                <img class="bg-white mx-4  w-44 my-5 rounded-lg py-3" src="" alt=""  >
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+                <h3>Hello, {{auth()->user()->role}}</h3>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+           
+            <a href="dashboard" class="text-x1 font-bold border-b-2 border-blue-500 block ml-4 px-2 py-1 hover:bg-blue-500 hover:text-white">Dashbord</a>
+
+            <a href="{{route('category.index')}}" class="text-x1 font-bold border-b-2 border-blue-500 block ml-4 px-2 py-1 hover:bg-blue-500 hover:text-white">Category</a>
+
+            <a href="{{route('notice.index')}}" class="text-x1 font-bold border-b-2 border-blue-500 block ml-4 px-2 py-1 hover:bg-blue-500 hover:text-white">Notice</a>
+
+            <a href="{{route('product.index')}}" class="text-x1 font-bold border-b-2 border-blue-500 block ml-4 px-2 py-1 hover:bg-blue-500 hover:text-white">Product</a><br>
+
+            <form action="{{route('logout')}}" method="POST">
+                @csrf
+                <input type="submit" value="LogOut" class="text-xl font-bold border -b-2 border-blue-500 block ml-4 px-2 py-1 hover:bg-blue-600 hover:text-white">
+            </form>
+
+            
         </div>
+           
+
+        <div class="p-4 flex-1 pl-56">
+            @yield('content')
+        </div>
+    </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\NoticeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Application;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +29,34 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    //route of category
+    Route::get('/category',[CategoryController::class,'index'])-> name('category.index');
+    Route::get('/category/create',[CategoryController::class,'create'])->name('category.create');
+    Route::post('/category/store',[CategoryController::class,'store'])->name('category.store');
+    Route::get('/category/{id}/edit',[CategoryController::class,'edit'])->name('category.edit');
+    Route::post('/category/{id}/update',[CategoryController::class,'update'])->name('category.update');
+    Route::get('/category/{id}/destroy',[CategoryController::class,'destroy'])->name('category.destroy');
+
+     //notice
+     Route::get('/notice',[NoticeController::class,'index'])->name('notice.index');
+     Route::get('/notice/create',[NoticeController::class,'create'])->name('notice.create');
+     Route::post('/notice/store',[NoticeController::class,'store'])->name('notice.store');
+     Route::get('/notice/{id}/edit',[NoticeController::class,'edit'])->name('notice.edit');
+     Route::post('/notice/{id}/update',[NoticeController::class,'update'])->name('notice.update');
+     Route::get('/notice/{id}/destroy',[NoticeController::class,'destroy'])->name('notice.destroy');
+
+     //product
+   
+    Route::get('/product',[ProductController::class,'index'])->name('product.index');
+    Route::get('/product/create',[ProductController::class,'create'])->name('product.create');
+    Route::post('/product/store',[ProductController::class,'store'])->name('product.store');
+    Route::get('/product/{id}/edit',[ProductController::class,'edit'])->name('product.edit');
+    Route::post('/product/{id}/update',[ProductController::class,'update'])->name('product.update');
+    Route::get('/product/{id}/destroy',[ProductController::class,'destroy'])->name('product.destroy');
+    
+
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
